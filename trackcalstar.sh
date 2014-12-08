4ed1e8eeb14ddc604c7ecf5afdff7063383ec9e8
Jessica Krick <jkrick@caltech.edu>
2014-12-08 13:36:39 -0800

    shell script called by cron which sets up and runs track_calstar_photometry.pro in both channels

---
+++ trackcalstar.sh
@@ -0,0 +1,6 @@
+#!/bin/tcsh
+ setenv IDL_PATH +/Users/jkrick/idlbin:+/Users/jkrick/idlbin/cm:+/Volumes/Nutella/Users/jkrick/idlbin:+/Volumes/Nutella/opt/idlutils/idlutils-v5_2_0/pro:+/Users/jkrick/idlbin/coyote:+/Users/jkrick/idlbin/idlastro/pro
+
+osascript -e 'tell application "Finder" to open location "nfs://ssceng1/ssc/Http/Docs/Instruments/IRAC/"'
+/Applications/exelis/idl83/bin/idl -e  "track_calstar_photometry, '/Users/jkrick/irac_warm/calstars/track_calstar_ch2.sav', 2, /make_plot,/binning"
+/Applications/exelis/idl83/bin/idl -e  "track_calstar_photometry, '/Users/jkrick/irac_warm/calstars/track_calstar_ch1.sav', 1, /make_plot,/binning"
